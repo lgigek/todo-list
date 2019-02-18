f85db67df8666e9d4399e1a4fb87b27999d866e9
Lucas Gigek <lucasgigek@gmail.com>
2019-02-18 11:33:47 -0300

    created a pattern to return messages and enhanced log

---
+++ web/routes/task_messages.py
@@ -0,0 +1,9 @@
+class TaskMessages:
+
+    incorrect_parameters = "Incorrect parameters"
+    invalid_status = "Invalid status. Please use 'to_do', 'doing' or 'done'"
+    duplicated = "Duplicated task name"
+    not_found = "Task not found"
+    created = "Task created"
+    updated = "Task updated"
+    deleted = "Task deleted"
--- web/routes/task_routes.py
+++ web/routes/task_routes.py
@@ -3,6 +3,7 @@ from flask import jsonify
 from flask import Blueprint
 from services import task_service
 from models.task import Task
+from routes.task_messages import TaskMessages
 import logging
 
 task = Blueprint('task', __name__)
@@ -15,34 +16,34 @@ def add():
     logging.info(f'HTTP Request to add a new task with JSON: {request_json}')
 
     if not _is_request_json_a_task(request_json):
-        logging.info('Incorrect parameters, task was not created')
-        return jsonify({'Message': 'Incorrect parameters'}), 400
+        logging.info('Incorrect parameters')
+        return jsonify({'Message': TaskMessages.incorrect_parameters}), 400
 
     new_task = Task(request_json['name'], request_json['description'], request_json['status'].lower())
     if task_service.is_registered(new_task.name):
-        logging.info('There is a task with the same name, task was not created')
-        return jsonify({'Message': 'Duplicated task'}), 400
+        logging.info('Duplicated task name')
+        return jsonify({'Message': TaskMessages.duplicated}), 400
 
     if not _is_status_valid(new_task.status):
-        logging.info(f'Task with invalid status ({new_task.status}), task was not created')
-        return jsonify({'Message': "Invalid value in 'status' field. Please use 'to_do', 'doing' or 'done'"})
+        logging.info('Invalid status')
+        return jsonify({'Message': TaskMessages.invalid_status})
 
     task_service.insert(new_task)
-    logging.info('Task created successful')
-    return jsonify({'name': new_task.name, 'description': new_task.description, 'status': new_task.status}), 201
+    logging.info('Task created')
+    return jsonify({'Message': TaskMessages.created}), 201
 
 
 @task.route('/get_by_name/<string:task_name>', methods=['GET'])
 def get_by_name(task_name):
     logging.info(f'HTTP Request to get a task by name with data: {request}')
-    logging.info(f'Looking for a task with name equal to "{task_name}"')
+    logging.info(f'Looking for a task with name "{task_name}"')
 
     task_found = task_service.get_by_name(task_name)
     if task_found is None:
         logging.info('Task not found')
-        return jsonify({'Message': 'Task not found'}), 404
+        return jsonify({'Message': TaskMessages.not_found}), 404
 
-    logging.info('Task found, returning its data')
+    logging.info('Returning task')
     return jsonify({'name': task_found['name'], 'description': task_found['description'],
                     'status': task_found['status']}), 200
 
@@ -55,8 +56,8 @@ def get_by_status(status):
     status = status.lower()
 
     if not _is_status_valid(status):
-        logging.info(f'Invalid status value ({status})')
-        return jsonify({'Message': "Invalid status. Please use 'to_do', 'doing' or 'done'"}), 400
+        logging.info('Invalid status')
+        return jsonify({'Message': TaskMessages.invalid_status}), 400
 
     tasks_found = task_service.get_by_status(status)
 
@@ -92,26 +93,25 @@ def update(task_name):
 
     if not task_service.is_registered(task_name):
         logging.info('Task not found')
-        return jsonify({'Message': 'Task not found'}), 404
+        return jsonify({'Message': TaskMessages.not_found}), 404
 
     if not _is_request_json_a_task(request_json):
         logging.info('Incorrect parameters')
-        return jsonify({'Message': 'Incorrect parameters'}), 400
+        return jsonify({'Message': TaskMessages.incorrect_parameters}), 400
 
     task_with_new_values = Task(request_json['name'], request_json['description'], request_json['status'].lower())
 
     if task_service.is_registered(task_with_new_values.name) and task_with_new_values.name != task_name:
-        logging.info('Task not updated, the name would be duplicated')
-        return jsonify({'Message': 'Task not updated, the name would be duplicated'}), 400
+        logging.info('Duplicated task name')
+        return jsonify({'Message': TaskMessages.duplicated}), 400
 
     if not _is_status_valid(task_with_new_values.status):
         logging.info('Invalid status')
-        return jsonify({'Message': "Invalid status. Please use 'to_do', 'doing' or 'done'"}), 400
+        return jsonify({'Message': TaskMessages.invalid_status}), 400
 
     task_service.update(task_name, task_with_new_values)
     logging.info('Task updated')
-    return jsonify({'name': task_with_new_values.name, 'description': task_with_new_values.description,
-                    'status': task_with_new_values.status}), 200
+    return jsonify({'Message': TaskMessages.updated}), 200
 
 
 @task.route('/delete/<string:task_name>', methods=['DELETE'])
@@ -121,11 +121,11 @@ def delete(task_name):
 
     if not task_service.is_registered(task_name):
         logging.info('Task not found')
-        return jsonify({'Message': 'Task not found'}), 404
+        return jsonify({'Message': TaskMessages.not_found}), 404
 
     logging.info('Task deleted')
     task_service.delete(task_name)
-    return jsonify({'Message': 'Task deleted'}), 200
+    return jsonify({'Message': TaskMessages.deleted}), 200
 
 
 def _is_request_json_a_task(request_json):
